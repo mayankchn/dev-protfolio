2747625547db1a46ccbb2b1340da4d7e427adc05
Mayank Chauhan <bobmayank1999@gmail.com>
2023-02-11 09:52:36 +0530

    enhancement-1 done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const Footer = () => {
     return (
         <footer className="text-secondary-dark mt-8">
-            <div className="container w-4/5 border mx-auto">
-                <div className="border flex flex-col gap-6 max-w-lg mx-auto px-20 py-12">
-                    <div className="flex gap-2 justify-evenly">
+            <div className="container w-4/5 mx-auto">
+                <div className="flex flex-col gap-6 max-w-lg mx-auto px-20 py-12">
+                    <div className="flex gap-2 justify-evenly flex-wrap">
                         <span className="w-10">
                             <a href="https://github.com/mayankchn" target="_blank"><img src="/github.png" alt="" className="w-full h-full aspect-square" /></a>
                         </span>
@@ -19,7 +19,7 @@ const Footer = () => {
                             <a href="https://twitter.com/themayankchn5" target="_blank"><img src="/twitter.png" alt="" className="w-full h-full aspect-square" /></a>
                         </span>
                     </div>
-                    <p className="self-center">Copyright ©2022 All rights reserved </p>
+                    <p className="self-center text-center">Copyright ©2022 All rights reserved </p>
                 </div>
             </div>
         </footer>
